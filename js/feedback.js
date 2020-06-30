ad793ea353cbbc8875348f9535576f7b7a61ee7f
Pasan-Kottearachchi <pasan.2018315@iit.ac.lk>
2020-06-30 09:22:31 +0530

    new feedback form added

--- js/feedback.js
+++ js/feedback.js
@@ -1,5 +1,8 @@
-function getName() {
-    return document.getElementById("cname").value;
+function getName1() {
+    return document.getElementById("cname1").value;
+}
+function getName2() {
+    return document.getElementById("cname2").value;
 }
 function getMail() {
     return document.getElementById("mail").value;
@@ -13,11 +16,13 @@ function getComment() {
 }
 
 function validateForm() {
-    var custName=getName();
+    var custName1=getName1();
+    var custName2=getName2();
     var custMail=getMail();
     var custNo=getNo();
     var custComment=getComment();
 
 
-    alert("Thank you "+custName+" for giving us a feedback! Your mail is "+custMail+" and the telephone number is "+custNo+". We value your comment "+custComment);
+    alert("Thank you "+custName1+" "+custName2+" for giving us a feedback! Your mail is "+custMail+" and the telephone number is "+custNo+
+        ". We value your comment "+custComment);
 }
